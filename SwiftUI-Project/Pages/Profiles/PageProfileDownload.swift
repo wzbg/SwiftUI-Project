c3b96046a29f6b8f2673e1a781f9e557d6c172d8
￠幻冰 <huanbing@wacai.com>
2020-02-05 14:09:08 +0800

    课程资源下载页

--- SwiftUI-Project/Pages/Profiles/PageProfileDownload.swift
+++ SwiftUI-Project/Pages/Profiles/PageProfileDownload.swift
@@ -9,8 +9,43 @@
 import SwiftUI
 
 struct PageProfileDownload: View {
+  @State private var isAnimating = false
+  
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    VStack(alignment: .leading, spacing: 0) {
+      SubPageTopTitle(title: "资源下载", subTitle: "仅需一次")
+      VStack(alignment: .leading, spacing: 30) {
+        VStack(alignment: .leading) {
+          Text("应用程序网址")
+            .modifier(RedNumber())
+          Text("https://itunes.apple.com/cn/app/id1392811165")
+            .modifier(RegularTitle())
+        }.modifier(SpringAnimationStyle(isAnimating: $isAnimating, delay: 0.3))
+        VStack(alignment: .leading) {
+          Text("课程素材网址")
+            .modifier(RedNumber())
+          Text("https://github.com/fzhlee")
+            .modifier(RegularTitle())
+        }.modifier(SpringAnimationStyle(isAnimating: $isAnimating, delay: 0.4))
+        VStack(alignment: .leading) {
+          Text("客服服务网址")
+            .modifier(RedNumber())
+          Text("https://www.hdjc8.com")
+          .modifier(RegularTitle())
+        }.modifier(SpringAnimationStyle(isAnimating: $isAnimating, delay: 0.5))
+        VStack(alignment: .leading) {
+          Text("QQ客服网址")
+            .modifier(RedNumber())
+          Text("3068527144")
+          .modifier(RegularTitle())
+        }.modifier(SpringAnimationStyle(isAnimating: $isAnimating, delay: 0.6))
+      }.padding(.top, 10)
+      .padding(.leading, 30)
+      Spacer()
+    }.modifier((SubPageContainer()))
+    .onAppear() {
+      self.isAnimating = true
+    }
   }
 }
 
